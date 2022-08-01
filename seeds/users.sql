19f60d7d685a903cab3ee26db26ddc06329deb63
JCPenne <jackcpalmer@icloud.com>
2022-08-01 12:09:13 -0700

    add migrations and seeds

---
+++ seeds/users.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (name, birth_year) VALUES ('Legolas',1982);
+INSERT INTO users (name, birth_year) VALUES ('Gimli',2879);
+INSERT INTO users (name, birth_year) VALUES ('Bilbo',2890);
+INSERT INTO users (name, birth_year) VALUES ('Aragorn',2931);
